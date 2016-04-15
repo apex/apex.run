76d0bf3552a80bab3e6cc99dd3d7e7f508173b76
Oskar Cieślik <oskar.cieslik@gmail.com>
2016-04-15 21:19:40 +0200

    White burger when on the header, black otherwise (#21)
    * Get rid of menu's white box, change color of burger depending on position
    
    * remove semicolon to match style
    
    * Remove parentheses that causing a bug

--- public/app.js
+++ public/app.js
@@ -21,3 +21,15 @@ if (!background) {
 // Body class for background.
 var el = document.getElementById('header-overlay')
 el.style.backgroundImage = 'url(public/images/'+background+'.jpg)'
+
+// Attach class to #menu element depending on page offset.
+document.addEventListener("DOMContentLoaded", chooseMenuColor)
+window.onscroll = chooseMenuColor
+
+function chooseMenuColor() {
+  var menuElement = document.getElementById('menu')
+  var menuTopOffset = 45
+  window.innerHeight < window.pageYOffset + menuTopOffset ?
+    menuElement.classList.remove('over-header') :
+    menuElement.classList.add('over-header')
+}
